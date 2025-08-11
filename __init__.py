ee9f6375161a5b72142e5d894bf935e3a6eb985d
Rafael Forte <rafael.somenzari@gmail.com>
2025-08-11 17:22:58 -0300

    Adicionado a funcao do get_domain

--- __init__.py
+++ __init__.py
@@ -1,12 +1,22 @@
 from googlesearch import search
 
 
-# TO-DO: Pegar um domínio de um file.txt para usar como parâmetro
 # TO-DO: Exportar as queries de um Excel. 
 # TO-DO: Fazer um regex para adicionar o dominio do site 
 
+def get_domain(domain_file="domain.txt"):
+    #Open the domain.txt and get the domain name from it
+    #TO-DO: REGEX, caso necessite
+    #TO-DO: Utilizar alguma flag no terminal para pegar referenciar esse file (ex: -f)
+    try:    
+        with open(domain_file, "r") as f:
+            domain_name = f.readline().strip()
+            print(domain_name)
+        return domain_name
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == '__main__':
-    query = ""
+    query = get_domain()
     for result in search(query, num_results=10, lang='en'):
         print(result)

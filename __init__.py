024cb66a89872ee53e1f1ca29554272e67c5083e
Rafael Forte <rafael.somenzari@gmail.com>
2025-08-14 16:24:10 -0300

    added search_domain fun

--- __init__.py
+++ __init__.py
@@ -5,18 +5,26 @@ from googlesearch import search
 # TO-DO: Fazer um regex para adicionar o dominio do site 
 
 def get_domain(domain_file="domain.txt"):
-    #Open the domain.txt and get the domain name from it
-    #TO-DO: REGEX, caso necessite
+    """
+    Open the domain.txt and get the domain name from it
+    """
     #TO-DO: Utilizar alguma flag no terminal para pegar referenciar esse file (ex: -f)
     try:    
-        with open(domain_file, "r") as f:
-            domain_name = f.readline().strip()
-            print(domain_name)
-        return domain_name
+        with open(domain_file, "r") as f: 
+            domain_name = [line.strip() for line in f if line.strip()]
+            return domain_name
     except Exception as e:
         print(f"Error: {e}")
 
+def search_domain(domains):
+    """
+    Search for a domain or a list of domain using Google Dorking
+    """
+    for domain in domains:
+        query = f"site: {domain}"
+        for result in search(query, num_results=5, lang="en"):
+            print(result)
+
 if __name__ == '__main__':
-    query = get_domain()
-    for result in search(query, num_results=10, lang='en'):
-        print(result)
+    search_domain(get_domain())
+    

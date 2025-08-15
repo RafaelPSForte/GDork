1b370a09b406c4f52e8eff66671cf9e718d80679
Rafael Forte <rafael.somenzari@gmail.com>
2025-08-15 17:04:22 -0300

    Adicionado nova funcionalidade de flags e trocado para duckgo

--- __init__.py
+++ __init__.py
@@ -1,30 +1,56 @@
-from googlesearch import search
+from ddgs import DDGS
+import argparse
 
 
 # TO-DO: Exportar as queries de um Excel. 
 # TO-DO: Fazer um regex para adicionar o dominio do site 
 
-def get_domain(domain_file="domain.txt"):
+def get_parse():
+    """
+    Get the flags from the terminal
+    """
+    parser = argparse.ArgumentParser(description="Script for GoogleDorking by Domains CLI inputs")
+    parser.add_argument("-f", required=False, type=str, help="Path for the domain file .txt")
+    parser.add_argument("--max-results", required=False, type=int,default=1, help="Specify how many results to return")
+    
+    return parser.parse_args()
+
+def get_domain(domain_file):
     """
     Open the domain.txt and get the domain name from it
     """
-    #TO-DO: Utilizar alguma flag no terminal para pegar referenciar esse file (ex: -f)
     try:    
-        with open(domain_file, "r") as f: 
+        with open(domain_file, "r") as f:
             domain_name = [line.strip() for line in f if line.strip()]
+            if not domain_name:
+                print("There is no domain name in the .txt file.")
+                return None
             return domain_name
     except Exception as e:
         print(f"Error: {e}")
 
-def search_domain(domains):
+def search_domain(domains, num_results = 5):
     """
     Search for a domain or a list of domain using Google Dorking
     """
-    for domain in domains:
-        query = f"site: {domain}"
-        for result in search(query, num_results=5, lang="en"):
-            print(result)
+    if not domains:
+        return
+    
+    with DDGS() as ddgs:
+        for domain in domains:
+            query = f"site:{domain}"
+            count = 0
+            for r in ddgs.text(query, max_results=num_results):
+                print(r['title'])
+                count+=1
+                if count >=num_results:
+                    break
+                
 
 if __name__ == '__main__':
-    search_domain(get_domain())
+    args = get_parse()
     
+    if args.f:
+        search_domain(get_domain(args.f), args.max_results)
+    else:
+        print("Domain file were NOT found, please add '-f path_to_file/domain_file.txt'")
